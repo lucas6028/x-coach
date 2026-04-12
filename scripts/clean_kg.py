30e37debfd8ded3ca64427c1b7372207bae403fd
haoping6028 <lucas60303@gmail.com>
2026-04-12 11:49:23 +0800

    modify mapping json path in clean kg

--- scripts/clean_kg.py
+++ scripts/clean_kg.py
@@ -8,7 +8,7 @@ import networkx as nx
 
 
 DEFAULT_GRAPH = Path("data/kg/squat_kg_v2.graphml")
-DEFAULT_MAPPING = Path("data/kg/docs/squat_canonical_mapping_v1.json")
+DEFAULT_MAPPING = Path("data/kg/squat_canonical_mapping_v1.json")
 DEFAULT_OUTPUT = Path("data/kg/squat_kg_v2.graphml")
 
 
